a934a3a438118175e68241cc7170b38e93c85359
Vinicius Muller de Freitas <viniciusmullerdefreitas309@gmail.com>
2024-07-28 22:37:02 -0300

    implements lexical analiser

---
+++ exemple.j
@@ -0,0 +1,15 @@
+START{ # Second test
+    # First test
+    INT var1;
+    FLOAT var2, var4;
+    BOOL var3;
+
+    var1 = 1;
+    var4 = 0.5;
+    
+    var2 = DIV(var1, 2);
+
+    OUT(var2);
+
+    END;
+}
---
+++ lexico.py
@@ -0,0 +1,150 @@
+# Token Table
+token_table = [
+    ("START", "start"),
+    ("END", "end"),
+    ("INT", "int"),
+    ("FLOAT", "float"),
+    ("BOOL", "bool"),
+    ("ADD", "add"),
+    ("SUB", "sub"),
+    ("MUL", "mul"),
+    ("DIV", "div"),
+    ("HIGH", "high"),
+    ("LOW", "low"),
+    ("EQUAL", "equal"),
+    ("NOT", "not"),
+    ("AND", "and"),
+    ("OR", "or"),
+    ("IN", "int"),
+    ("OUT", "out"),
+    ("IF", "if"),
+    ("ELSE", "else"),
+    ("WHILE", "while"),
+    ("FOR", "for"),
+    ("TRUE", "true"),
+    ("FALSE", "false"),
+    ("(", "par_esq"),
+    (")", "par_dir"),
+    ("{", "cha_esq"),
+    ("}", "cha_dir"),
+    (",", "virgula"),
+    (";", "ponto-virgula"),
+    ("=", "associasao")
+]
+
+# Symbol list
+symbols = ['(', ')', '{', '}', ',', ';', '=', '"']
+
+# Get the contents of the file
+def read_file(filepath):
+    try:
+        with open(filepath, 'r') as file:
+            return file.readlines()
+    except FileNotFoundError:
+        return None
+
+def keywords_var(count, line, token_list, line_num, token_table):
+    
+    # Make the table into a dictionary
+    token_dict = dict(token_table)
+    
+    # List o capture characters
+    char_list = []
+    
+    # Mount the string
+    while(line[count].isalpha() or line[count].isdigit()):
+        char_list.append(line[count])
+        count += 1
+    string = ''.join(char_list)
+    
+    # See if its a keyword and put on the list
+    if(string in token_dict):
+        token_list.append((string, token_dict[string], line_num))
+    else:
+        token_list.append((string, 'id', line_num))
+    
+    # Return the pointer
+    return count
+
+def symbol_token(char, token_list, line_num, token_table):
+    
+    # Make the table into a dictionary
+    token_dict = dict(token_table)
+    
+    if(char in token_dict):
+        token_list.append((char, token_dict[char], line_num))
+
+def number_token(count, line, token_list, line_num, token_table):
+    
+    # Make the table into a dictionary
+    token_dict = dict(token_table)
+    
+    # List o capture characters
+    num_list = []
+    
+    # Mount the string
+    while(line[count].isdigit() or line[count] == '.'):
+        num_list.append(line[count])
+        count += 1
+    number = ''.join(num_list)
+    
+    # Put the number on token list
+    token_list.append((number, 'num', line_num))
+    
+    # Return the pointer 
+    return count
+    
+
+def main():
+    tokens = []
+    code = read_file('exemple.j')
+    line_num = 1
+    
+    # Get each line of the code
+    for line in code:
+        
+        # Control variables
+        lenght = len(line)
+        count = 0
+        
+        while(count < lenght):
+            
+            # Check if its a space
+            if(line[count].isspace()):
+                count += 1
+                continue
+            
+            # Check if its a line break
+            elif(line[count] == "\n"):
+                break
+            
+            # Check if its a comment
+            elif(line[count] == "#"):
+                while(count < lenght and line[count] != "\n"):
+                    count += 1
+                
+            # Check if its a char
+            elif(line[count].isalpha()):
+                count = keywords_var(count, line, tokens, line_num, token_table) - 1
+            
+            # Check if its an symbol
+            elif(line[count] in symbols):
+                symbol_token(line[count], tokens, line_num, token_table)
+                
+            # Check if its an number
+            elif(line[count].isdecimal()):
+                count = number_token(count, line, tokens, line_num, token_table) - 1
+            
+            else:
+                print("Erro encontrado na linha", line_num)
+            
+            count += 1
+        
+        # Next line
+        line_num += 1
+    
+    print('\n', tokens)      
+            
+        
+        
+main()
